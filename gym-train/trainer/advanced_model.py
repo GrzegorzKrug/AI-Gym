332afced1df1ce613cd2a9a09b947b5181a5986a
Grzegorz Krug <grzesiekk321@gmail.com>
2022-08-14 20:42:23 +0200

    Trainer + flat model

---
+++ gym-train/trainer/advanced_model.py
@@ -0,0 +1,288 @@
+import datetime
+import numpy as np
+import os
+import pickle
+
+from keras.layers import (
+    Dense, Flatten, Dropout,
+    Conv2D, MaxPool2D,
+    Softmax, Input, concatenate,
+)
+
+from keras.models import Sequential, load_model, Model
+from keras.initializers import RandomUniform
+from keras.callbacks import TensorBoard
+from keras.utils import plot_model
+from keras.optimizers import Adam
+
+from collections import deque
+from copy import deepcopy
+
+
+class FlatModel:
+    def __init__(
+            self,
+            /,
+            node_shapes=None,
+            compiler=None,
+            memory_size=10000,
+
+            alpha=0.99,
+            beta=0.99,
+            gamma=0.99,
+            name=None,
+            load_model=True,
+            save_model=True,
+            save_dir="models",
+    ):
+        dt = datetime.datetime.timetuple(datetime.datetime.now())
+        self.runtime_str = f"Y{dt.tm_year}M{dt.tm_mon:>02}D{dt.tm_mday:>02}-" \
+                           f"{dt.tm_hour:>02}:{dt.tm_min:>02}:{dt.tm_sec:>02}"
+        self.name = str(name)
+
+        self.node_shapes = node_shapes
+        self.compiler = compiler
+        self.memory_size = memory_size
+
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+
+        self.memory = deque(maxlen=memory_size)
+        self.model = None
+        self.model_name = f"{(type(self).__qualname__)}_{self.name}"  # Captures class name
+
+        self.allow_load = load_model
+        self.allow_save = save_model
+        self.save_dir = str(save_dir)
+
+        if self.allow_load:
+            self.load_model()
+        else:
+            self.make_model()
+            print(f"New model: {self.model_name}")
+
+        self.save_summary()
+
+    def make_model_dir(self):
+        os.makedirs(os.path.dirname(self.path_params), exist_ok=True)
+
+    @property
+    def params(self):
+        """
+        Specify what params to save and load
+        Returns:
+
+        """
+        return self.node_shapes, self.compiler
+
+    @params.setter
+    def params(self, new_params):
+        """
+        Specify what params to save and load
+        Returns:
+
+        """
+        self.node_shapes, self.compiler = new_params
+
+    def load_model(self):
+        print(f"Loading model {self.name}")
+        self.load_params()
+        self.make_model()
+        self.load_model_weights()
+        self.load_memory()
+
+    def load_params(self):
+        try:
+            pars = np.load(self.path_params, allow_pickle=True)
+            self.params = pars
+            print(f"Loading params {self.path_params}")
+            print(self.params)
+        except FileNotFoundError:
+            print(f"Not found params to load {self.path_params}")
+
+    def save_params(self):
+        arr = np.array(self.params, dtype=object)
+        np.save(self.path_params, arr)
+        print(f"Saving params {self.path_params}")
+
+    def load_memory(self, drop_last=0, path_override=None):
+        try:
+            if path_override:
+                path = path_override
+            else:
+                path = self.path_memory
+
+            mem = np.load(path, allow_pickle=True)
+            print(f"Loaded memory: {mem.shape} -> {mem.shape[0] - drop_last} samples")
+            for ind, sample in enumerate(mem):
+                if ind + drop_last >= len(mem):
+                    break
+                self.add_memory(sample)
+            print(f"Loading memory {self.path_memory}")
+
+        except FileNotFoundError:
+            print(f"Not found memory file {self.path_memory}")
+
+    def save_memory(self):
+        mem = np.array(self.memory, dtype=object)
+        np.save(self.path_memory, mem)
+        print(f"Saving memory {self.path_memory}, size: {len(self.memory)}")
+
+    @property
+    def _main_path(self):
+        """Path to main folder for this model"""
+        return f"{self.save_dir}{os.path.sep}{self.model_name}"
+
+    @property
+    def path_model_dir(self):
+        return os.path.abspath(self._main_path) + os.path.sep
+
+    @property
+    def path_model_weights(self):
+        return os.path.abspath(f"{self._main_path}") + os.path.sep
+
+    @property
+    def path_params(self):
+        return os.path.abspath(f"{self._main_path}{os.path.sep}model{os.path.sep}params-model.npy")
+
+    @property
+    def path_memory(self):
+        return os.path.abspath(f"{self._main_path}{os.path.sep}model{os.path.sep}memory.npy")
+
+    def save_all(self):
+        """Save model, params and memmory"""
+        if not self.allow_save:
+            print("Saving is not allowed")
+            return None
+
+        # self.make_model_dir()
+        self.save_memory()
+        self.save_params()
+        self._save_weights(self.model, self.path_model_weights)
+        # plot_model(self.model, to_file=self.path_model + os.sep + "model.png")
+
+    def save_summary(self):
+        self.make_model_dir()
+
+        with open(self.path_model_dir + "summary.txt", 'wt') as fh:
+            fh.write(f"Model: {self.model_name}\n")
+            self.model.summary(print_fn=lambda x: fh.write(x + '\n'))
+
+        with open(self.path_model_dir + "params_preview.txt", 'wt') as fh:
+            fh.write(f"Model: {self.model_name}\n\n")
+            pars, optimizer = self.params
+            for par in pars:
+                fh.write(f"{par}\n")
+
+            fh.write(f"\n")
+            fh.write(f"{optimizer}\n")
+
+        return True
+
+    def save(self):
+        self.save_model()
+
+    def load(self):
+        self.load_model()
+
+    @staticmethod
+    def _save_weights(mod, path):
+        while True:
+            try:
+                mod.save_weights(path)
+                print(f"Saving weights {path}")
+                break
+            except OSError:
+                time.sleep(0.2)
+            except AttributeError as err:
+                print(f"Can not save weights: {err} to {path}")
+                break
+
+    @staticmethod
+    def _load_weights(mod, path):
+        check_path = path + ".index"
+        f1 = os.path.isfile(check_path)
+        if f1:
+            while True:
+                try:
+                    mod.load_weights(path)
+                    print(f"Loaded weights: {path}")
+                    break
+                except OSError:
+                    time.sleep(0.2)
+        else:
+            print(f"No weights: {path}")
+
+    # def choose_action_list(self, States):
+    #     probs = self.policy.predict([States])
+    #     actions = np.array([np.random.choice(settings.ACTION_SPACE, p=p) for p in probs])
+    #     return actions
+
+    def load_model_weights(self):
+        self._load_weights(self.model, self.path_model_weights)
+
+    def add_memory(self, data):
+        self.memory.append(data)
+
+    def make_model(self):
+        print(f"Creating dqn model {self.name}")
+        layers = []
+        CFG = deepcopy(self.node_shapes)
+        print(CFG)
+        for cfg in CFG:
+            node = cfg.pop('node', None)
+            args = cfg.pop('args', ())
+            kwargs = cfg
+
+            if not node:
+                raise ValueError("Empty node value")
+            node = node.lower()
+            print(f"Adding {node:>10}, Args: {args}, Kwargs: {kwargs}")
+
+            if node == 'conv2d':
+                lay = Conv2D(*args, **kwargs)
+            elif node == 'dense':
+                lay = Dense(*args, **kwargs)
+            elif node == "maxpool":
+                lay = MaxPool2D(*args, **kwargs)
+            elif node == "dropout":
+                lay = Dropout(*args, **kwargs)
+            elif node == "input":
+                lay = Input(*args, **kwargs)
+            elif node == 'flatten':
+                lay = Flatten()
+            else:
+                raise ValueError(f"Unknown layer type: {node}")
+
+            layers.append(lay)
+
+        model = Sequential(layers)
+        # model.compile(**self.compiler)
+        model.compile(optimizer='sgd', loss='mse', metrics=['accuracy'])
+        self.model = model
+
+    def predict(self, *args, **kwargs):
+        return self.model.predict(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    NODES_CONFIG = [
+            {'node': 'input', 'args': (4,)},
+            {'node': 'dense', 'args': (4,), 'activation': 'relu'},
+            {'node': "dropout", 'args': (0.05,)},
+            {'node': 'dense', 'args': (100,), 'activation': 'relu'},
+            {'node': "dropout", 'args': (0.05,)},
+            {'node': 'dense', 'args': (100,), 'activation': 'relu'},
+            {'node': 'dense', 'args': (4,), 'activation': 'softmax'},
+    ]
+
+    COMPILER = {
+            'optimizer': 'adam',
+            'loss': 'mse',
+            'metrics': ['accuracy'],
+    }
+
+    mod = FlatModel(NODES_CONFIG, compiler=COMPILER, name="test")
+    mod.save_all()
+    print(mod.runtime_str)
---
+++ gym-train/trainer/trainer.py
@@ -0,0 +1,425 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import random
+import numpy as np
+import numba
+import time
+import gym
+import os
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Concatenate, Dropout, Input
+from tensorflow.keras.optimizers import Adam
+
+
+
+class Trainer:
+    """"""
+    """
+    Config
+        Lower case keys!
+    0: One agent and one env.
+    1: One agent and many envs.
+    2: Many agents and many envs.
+    """
+    configs = {
+            k.lower(): v for k, v
+            in {
+                    'OneToOne': 0,
+                    'OneToMany': 1,
+                    'ManyToMany': 2,
+            }.items()
+    }
+
+    # configs = {k.lower(): v for k, v in configs.items()}
+
+    def __init__(self,
+                 model, environments,
+                 envs_n=10,
+                 input_size=2, output_size=2, action_size=1,
+                 memory_max_samples=5000,
+                 batch_size=200, train_size=2000,
+                 train_each_step=False, split_train=False,
+                 config='OneToOne',
+                 notify_win=2,
+                 **kw
+                 ):
+        """
+
+        Args:
+            model:
+            environments:
+            envs_n:
+            input_size:
+            output_size:
+            action_size:
+            memory_max_samples:
+            batch_size:
+            train_size:
+            train_each_step:
+            split_train:
+            config:
+                keys are case insensitive
+                OneToOne - One agent one env
+                OneToMany - One agent, many envs
+                ManyToMany - (Not implemented) Many agents with many envs
+            notify_win:
+                0 - None
+                1 - Cumulated data
+                2 - Every single one
+            **kw:
+        """
+        if len(kw) > 0:
+            print("Uncaught kwargs")
+            print(kw)
+
+        "Training settings"
+        self.model = model
+        self.envs = environments
+        self.config = self.configs.get(config.lower(), 0)
+        self.config_name = config
+        self.input_size = input_size
+        self.output_size = output_size
+        self.action_size = action_size
+
+        self.min_train_samples = 10
+        self.batch_size = batch_size
+        self.train_size = train_size
+        # self.max_samples = 200
+
+        "Config"
+        if self.config == 0:
+            self.envs_n = 1
+        elif self.config == 1:
+            self.envs_n = envs_n
+        else:
+            raise NotImplemented(f"Not implemented config {self.config_name}")
+        print(f"Starting with config: {self.config} ({self.config_name})")
+
+        "Memory settings (fitting)"
+        self.memory = np.zeros((memory_max_samples, (input_size * 2 + 3)))
+        self.memory_write_index = 0
+        self.memory_last_index = memory_max_samples - 1
+        self.memory_fully_writen = False
+
+        "Last state memory"
+        self.states = np.zeros((self.envs_n, self.input_size), dtype=np.float32)
+        # self.actions = np.zeros((self.envs_n, self.action_size), dtype=np.float32)
+        # self.rewards = np.zeros((self.envs_n, 1), dtype=np.float32)
+        self.alive_envs = set(range(envs_n))
+
+        self.notify = notify_win
+
+        "Additional trainer setup"
+        self.post_init_called = False
+        self.__post_init__()
+        self.reset()
+
+    def __post_init__(self):
+        self._init_set_step_func()
+        self._init_set_train_func()
+
+        self.post_init_called = True
+
+    def _init_set_step_func(self):
+        cf = self.config
+        if cf == 0:
+            self._inner_step = self._inner_step_single
+        elif cf == 1:
+            self._inner_step = self._inner_step_one_agent_many_envs
+        else:
+            self._inner_step = self._inner_step_many_agents_many_envs
+
+    def _init_set_train_func(self):
+        cf = self.config
+        if cf == 0:
+            self._inner_train = self._inner_train_single
+        elif cf == 1:
+            self._inner_train = self._inner_train_single
+            # self._inner_train = self._inner_train_one_agent_many_envs # Redundant
+        else:
+            self._inner_train = self._inner_train_many_agents_many_envs
+
+    def reset(self):
+        self.states = np.zeros((self.envs_n, self.input_size), dtype=np.float32)
+        # self.actions = np.zeros((self.envs_n, self.action_size), dtype=np.int32)
+        # self.rewards = np.zeros((self.envs_n, 1), dtype=np.float32)
+        self.alive_envs = set(range(envs_n))
+
+        for i in range(self.envs_n):
+            # print(f"I:{i}")
+            state = self.envs[i].reset()
+            self.states[i] = state
+
+    def start_training(self, epochs=1, max_iters=10,
+                       exploration_type='cos',
+                       exploration_ratio=0.3,
+                       min_exploration=1e-2,
+                       ):
+        for r in range(epochs):
+            "Iterate over epochs"
+            self.reset()  # Clear environment at start
+            current_explore_ratio = np.cos(r / (epochs - 1) * np.pi * 2.5) * exploration_ratio
+            current_explore_ratio = np.abs(current_explore_ratio)
+            if current_explore_ratio < min_exploration:
+                current_explore_ratio = min_exploration
+
+            print(f"Epoch: {r}, exploration: {current_explore_ratio}")
+            for n in range(max_iters):
+                if len(self.alive_envs) <= 0:
+                    break
+                rnd_act = True if current_explore_ratio >= np.random.random() else False
+                self._inner_step(random_action=rnd_act)
+
+            self._inner_train()
+
+    def _inner_step(self, random_action=False):
+        """
+        Abstraction
+            random_action [boolean] - model choice or random action
+        """
+        raise NotImplemented("This is prototype")
+
+    def _inner_train(self):
+        """ Abstraction """
+        raise NotImplemented("This is prototype")
+
+    def _inner_step_single(self, random_act=False):
+        old_state = self.states[0]
+        if random_act:
+            actions = np.random.randint(0, self.action_size, (self.envs_n,))
+        else:
+            actions = self.predict()[0]
+        new_state, reward, end, info = self.envs[0].step(actions)
+        sample = np.concatenate([old_state, new_state, actions[0], [reward], [end]],
+                                dtype=np.float32)
+        self.add_memory(sample)
+
+    def predict(self):
+        """DL Q-learning"""
+        qvals = self.model.predict(self.states, verbose=False)
+        actions = np.argmax(qvals, axis=1)
+        return actions
+
+    def get_training_samples(self):
+        # print("Random samples:")
+        if self.memory_fully_writen:
+            k_samples = self.train_size
+            inds = random.sample(range(self.memory_last_index), k_samples)
+            # samples = np.random.sample(self.memory, axis=0)
+        # elif self.memory_write_index <= self.min_samples:
+        #     print("return none")
+        #     return None
+        else:
+            k_samples = self.memory_write_index
+            inds = random.sample(range(self.memory_write_index), k_samples)
+            # samples = np.random.sample(self.memory[self.memory_write_index - 1], axis=0)
+        samples = self.memory[inds, :]
+        # print(f"inds: {inds}")
+        # print(f"Selected samples for fit: {samples.shape}")
+        # print(samples)
+        # print("aasdasdasdasd")
+        return samples
+
+    @property
+    def sample_indexes(self):
+        return self.input_size, self.input_size * 2,
+
+    def _inner_train_single(self):
+        if self.memory_fully_writen or self.memory_write_index > self.min_train_samples:
+            train_data = self.get_training_samples()
+        else:
+            return None
+        ind1, ind2 = self.sample_indexes
+        old_state = train_data[:, :ind1]
+        new_state = train_data[:, ind1:ind2]
+        action_inds = train_data[:, ind2].astype(np.int32)
+        reward = train_data[:, ind2 + 1].reshape((-1, 1))
+        end = train_data[:, ind2 + 2]
+
+        # print("FIT Y")
+
+        # action = np.zeros((action_inds.shape[0], self.output_size * self.action_size))
+        # action[:, action_inds] = 1
+        # for aci, ind in enumerate(action_inds):
+        # action[aci, ind] = 1
+
+        current_qvals = self.model.predict(old_state, verbose=False)
+        future_qvals = self.model.predict(new_state, verbose=False)
+        future_max = np.max(future_qvals, axis=1).reshape(-1, 1)
+
+        gamma = 0.9
+        Y = current_qvals
+        for ind in np.argwhere(end > 0)[0]:
+            Y[ind, action_inds[ind]] = reward[ind]
+
+        for ind in np.argwhere(end <= 0)[0]:
+            aind = action_inds[ind]
+            Y[ind, aind] = reward[ind] + gamma * (future_max[ind] - Y[ind, aind])
+        # print("Y:")
+        # print(Y.shape)
+        # print(Y[:5, :])
+        # print("State:")
+        # print(old_state[:5, :])
+        self.model.fit(old_state, Y, batch_size=self.batch_size)
+
+    def _inner_step_one_agent_many_envs(self, random_action=False):
+        old_states = self.states[list(self.alive_envs)]
+
+        if random_action:
+            actions = np.random.randint(0, self.action_size, (self.envs_n,))
+        else:
+            actions = self.model.predict(old_states, verbose=False)
+            actions = np.argmax(actions, axis=1)
+
+        rewards_text = ""
+
+        for current_i, (act, env_ind) in enumerate(zip(actions, list(self.alive_envs))):
+            "Loop over environments"
+            new_state, reward, end, info = self.envs[env_ind].step(act)
+            # reward = self.tweak_reward(None, new_state, reward)
+
+            if self.notify == 2:
+                if reward >= 0:
+                    rewards_text += f"{reward}, "
+            sample = np.concatenate([old_states[current_i, :], new_state, [act], [reward], [end]],
+                                    dtype=np.float32)
+            if end:
+                self.alive_envs.remove(env_ind)
+
+            self.add_memory(sample)
+        if len(rewards_text) > 0 and self.notify == 2:
+            print(f"End of step: {rewards_text}")
+
+    # def _inner_train_one_agent_many_envs(self):
+    #     if self.memory_fully_writen or self.memory_write_index > self.min_samples:
+    #         train_data = self.get_training_samples()
+    #     else:
+    #         return None
+    #     ind1, ind2 = self.sample_indexes
+    #     old_state = train_data[:, :ind1]
+    #     new_state = train_data[:, ind1:ind2]
+    #     action_inds = train_data[:, ind2].astype(np.int32)
+    #     reward = train_data[:, ind2 + 1].reshape((-1, 1))
+    #     end = train_data[:, ind2 + 2]
+    #
+    #     # print("FIT Y")
+    #
+    #     action = np.zeros((action_inds.shape[0], self.output_size * self.action_size))
+    #     # action[:, action_inds] = 1
+    #     for aci, ind in enumerate(action_inds):
+    #         # print(aci, ind)
+    #         action[aci, ind] = 1
+    #
+    #     future_qvals = self.model.predict(new_state)
+    #     future_max = np.max(future_qvals, axis=1).reshape(-1, 1)
+    #
+    #     gamma = 0.9
+    #     Y = gamma * (future_max + reward)
+    #     Y[end] = reward[end]
+    #
+    #     print("actions", action_inds)
+    #     self.model.fit(old_state, Y)
+
+    def _inner_step_many_agents_many_envs(self):
+        """"""
+        raise NotImplemented
+
+    def _inner_train_many_agents_many_envs(self):
+        """"""
+        raise NotImplemented
+
+    def add_memory(self, sample):
+        self.memory[self.memory_write_index] = sample
+        if self.memory_write_index >= self.memory_last_index:
+            self.memory_write_index = 0
+            self.memory_fully_writen = True
+        else:
+            self.memory_write_index += 1
+
+    def tweak_reward(self, prev_state, state, reward):
+        # print(state)
+        reward += np.abs(state[1]) * 20
+        return reward
+
+
+def simple_model(in_shape, out_shape):
+    inp = Input(in_shape)
+    lay = Dense(100, activation='relu')(inp)
+    lay = Dense(100, activation='relu')(lay)
+    out = Dense(out_shape, activation='linear')(lay)
+
+    model = Model(inputs=[inp], outputs=[out])
+    model.compile(optimizer=Adam(learning_rate=1e-3),
+                  loss='mse',
+                  metrics=['accuracy'])
+    return model
+
+
+def simple_env(n=1):
+    envs = [gym.make('LunarLander-v2') for _ in range(n)]
+    envs = [gym.make('MountainCar-v0') for _ in range(n)]
+    return envs
+
+
+if __name__ == "__main__":
+    envs_n = 15
+    input_size, output_size = 8, 4  # Lunar
+    input_size, output_size = 2, 3  # Mountain Car
+    model = simple_model(input_size, output_size)
+    if os.path.isfile("mod1.weights"):
+        pass
+        # model.load_weights("mod1.weights")
+
+    envs = simple_env(envs_n)
+    trening = Trainer(model, envs,
+                      config='onetomany', envs_n=envs_n,
+                      input_size=input_size, output_size=output_size, action_size=1,
+                      memory_max_samples=envs_n * 400,
+                      batch_size=100, train_size=envs_n * 100,
+                      notify_win=2,
+                      )
+
+    np.set_printoptions(suppress=True, precision=4)
+
+    trening.start_training(epochs=5, max_iters=200, exploration_type='sin', exploration_ratio=0.4)
+    rewards = tuple(sample[3] for sample in trening.memory)
+    rewards = np.array(rewards)
+    print("Rewards")
+    print(str(rewards))
+    plt.hist(rewards, bins=50)
+    # print()
+    model.save_weights("mod1.weights")
+
+    "POST Train Render"
+    game = simple_env(1)[0]
+    end = False
+    state = game.reset()
+    game.render()
+    time.sleep(5)
+
+    rewards = np.zeros_like(rewards)[:201]
+    i = 0
+    while not end:
+        state = state.reshape(1, -1)
+        # print(state)
+        game.render()
+        qvals = model.predict(state, verbose=False)
+        act = np.argmax(qvals)
+        print(state, act, qvals)
+        state, reward, end, info = game.step(act)
+        time.sleep(0.01)
+        rewards[i] = reward
+        i += 1
+
+    print(f"max i: {i}")
+    rewards = rewards[:i]
+
+    plt.figure()
+    plt.hist(rewards, bins=50)
+    plt.title("Visual rewards")
+    plt.show()
+    time.sleep(10)
+
+    # print(tf.)
+    # print(tf.test.is_gpu_available())
