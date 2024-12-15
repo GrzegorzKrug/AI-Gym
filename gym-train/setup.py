f1927e86031fb5971eb8630c964dfb43a3424dcd
Grzegorz Krug <grzesiekk321@gmail.com>
2024-12-15 17:36:09 +0100

    Create setup.py

---
+++ gym-train/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='GymTrainer',
+      version='1.0',
+      description='Flexible environment to run multiple agents easily',
+      author='Grzegorz Krug',
+      author_email='',
+      url='',
+      packages=['trainer'],
+      )
